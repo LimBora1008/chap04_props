07f66882d7b2d50e0c2a68929d7d4a51c98c96cd
LimBora1008 <cat_say@naver.com>
2023-06-22 17:48:26 +0900

    0622

---
+++ src/component/Post.js
@@ -0,0 +1,10 @@
+import '../App.css';
+import Comment from './Comment';
+
+export default function Post(props) {
+    const { title, content, author } = props;
+    
+    return (
+        <Comment title = {title} content = {content} author = {author} />
+    );
+}
